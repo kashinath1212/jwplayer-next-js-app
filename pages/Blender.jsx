d71b094c0d99ccef60f9ee46fe706abc17a96a8d
kashinath1212 <kashinathswarnkar@appscrip.co>
2022-11-02 17:10:35 +0530

    sdfsa

--- pages/Blender.jsx
+++ pages/Blender.jsx
@@ -28,16 +28,6 @@ function Blender(props) {
 
   return (
     <div className="App">
-      <Head>
-        <title>Blender app</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Jwplayer Blender Preview App" />
-        <meta property="og:url" content='https://cdn.jwplayer.com/v2/media/h7pkBUcC/poster.jpg?width=720' />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content='Jwplayer Blender app' />
-        <meta property="og:description" content="Jwplayer Blender Preview App" />
-        <meta property="og:image" content='https://assets-jpcust.jwpsrv.com/thumbnails/9pndcdhu-720.jpg' />
-      </Head>
       <DynamicBlur>
         <BlurBgImage>
           <Image src='https://cdn.jwplayer.com/v2/media/LEBW145Q/poster.jpg?width=720' width='1500' height='1500' alt='alk' />
--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
         <title>Blender app</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Jwplayer Blender Preview App" />
-        <meta property="og:url" content='https://cdn.jwplayer.com/v2/media/h7pkBUcC/poster.jpg?width=720' />
+        <meta property="og:url" content='https://cdn.jwplayer.com/v2/media/awWEFyPu/poster.jpg?width=720' />
         <meta property="og:type" content="website" />
         <meta property="og:title" content='Jwplayer Blender app' />
         <meta property="og:description" content="Jwplayer Blender Preview App" />
-        <meta property="og:image" content='https://assets-jpcust.jwpsrv.com/thumbnails/9pndcdhu-720.jpg' />
+        <meta property="og:image" content='https://assets-jpcust.jwpsrv.com/thumbnails/9azldgqg-720.jpg' />
       </Head>
 
       <main>
